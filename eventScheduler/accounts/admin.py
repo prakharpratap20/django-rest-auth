3b6eb6befa738c84bcdb10760c9b256b621cc24c
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-08-29 13:21:19 +0530

    comments

--- eventScheduler/accounts/admin.py
+++ eventScheduler/accounts/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import UserProfile
 
 # Register your models here.
+
+
+# Register the UserProfile model with the admin site.
 admin.site.register(UserProfile)
 
+# Change the admin site header to "events scheduler".
 admin.site.site_header = "events scheduler"
--- eventScheduler/accounts/models.py
+++ eventScheduler/accounts/models.py
@@ -5,8 +5,13 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
+    """
+    A model to represent a user profile in the system.
+    This model extends the User model from Django.
+    """
     user = (
-        models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile"),
+        models.OneToOneField(User, on_delete=models.CASCADE,
+                             related_name="profile"),
     )
     description = models.TextField(blank=True, null=True)
     location = models.CharField(max_length=30, blank=True)
