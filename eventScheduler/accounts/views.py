449e87de9ec61276d494aa11f5eacca886a4a07d
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-08-29 13:16:19 +0530

    views bug fixed

--- eventScheduler/accounts/views.py
+++ eventScheduler/accounts/views.py
@@ -9,16 +9,28 @@ from .serializers import UserProfileSerializer
 
 
 class UserProfileListCreateView(ListCreateAPIView):
+    """
+    List all user profiles or create a new user profile.
+    Only authenticated users can create a new user profile.
+    """
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
+        """
+        Associate the user profile with the authenticated user when creating a new user profile.
+        """
         user = self.request.user
         serializer.save(user=user)
 
 
 class UserProfileDetailView(RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a user profile.
+    Only authenticated users can update or delete a user profile
+    and only the owner of the profile can update or delete it.
+    """
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = [IsOwnerProfileOrReadOnly, IsAuthenticated]
